2b52545fee923383ca792ccaf1c3fe6243b33a8c
brupeps <79551322+brupeps@users.noreply.github.com>
2021-03-02 23:14:33 -0300

    Add files via upload

---
+++ aula2.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+
+int main(int argc, char *argv[]) {
+    int i = 0, n = 0; 
+    printf("Digite dois valores na tela:\n");
+    scanf("%i",&i);
+    scanf("%i",&n);gi
+    printf("Resultado: %i",n+i);
+    //system("pause");
+}
